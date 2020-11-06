e2473a9819a8fd53dc13099ab0ce38d3d9d14a1a
Dokas10 <andremar1000@gmail.com>
2020-11-06 15:02:30 +0000

    Initial commit

---
+++ app/src/main/java/com/andre/projects/animemanager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.andre.projects.animemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
